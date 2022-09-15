dd9589706061b7175e4f6788c84933213598130a
Suhaib <sszia@swiss-te.ch>
2022-09-15 20:40:41 +0500

    SearchBar component added

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import VideoDetail from "./VideoDetail";
+import ChannelDetail from "./ChannelDetail";
+import SearchFeed from "./SearchFeed";
+import SearchBar from "./SearchBar";
+export {
+    Navbar,
+    Feed,
+    VideoDetail,
+    ChannelDetail,
+    SearchFeed,
+    SearchBar
+}

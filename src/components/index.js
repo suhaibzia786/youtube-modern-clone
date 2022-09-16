dc2e868ab543579aaae7fa66f3dab835c7750cd4
Suhaib <sszia@swiss-te.ch>
2022-09-16 15:02:10 +0500

    Sidebar dynamic and video card and channel card design and dynamics completed

--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,20 @@ import VideoDetail from "./VideoDetail";
 import ChannelDetail from "./ChannelDetail";
 import SearchFeed from "./SearchFeed";
 import SearchBar from "./SearchBar";
+import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
+
 export {
     Navbar,
     Feed,
     VideoDetail,
     ChannelDetail,
     SearchFeed,
-    SearchBar
+    SearchBar,
+    Sidebar,
+    Videos,
+    VideoCard,
+    ChannelCard
 }
